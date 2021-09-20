f7e190bed127b6070705bba55a4bd678eb8467f9
Hussien Khayoon <hussien@vendorhero.com>
2021-09-20 09:58:47 -0400

    commit to run workflow again

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,5 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const htmlPlugin = new HtmlWebpackPlugin({
   template: path.join(__dirname, 'src', 'index.html'),
